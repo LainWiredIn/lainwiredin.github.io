9fafa5aaf9b5eff7f323461a8115b82a23a11c74
LainWiredIn <73900947+LainWiredIn@users.noreply.github.com>
2025-05-22 11:50:11 +0000

    Deploying to gh-pages from @ LainWiredIn/lainwiredin.github.io@68323435e6e533b183524074b45e2d6c6de4a5ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
@@ -372,52 +365,7 @@ ninja.data = [{
             },},{id: "news-thrilled-to-share-that-our-paper-screening-of-oral-potentially-malignant-disorders-and-oral-cancer-using-deep-learning-models-has-been-accepted-in-scientific-reports-a-nature-portfolio-journal-2025-this-research-was-part-of-my-internship-at-the-applied-ai-research-centre-inai-iiit-hyderabad-a-collaborative-initiative-by-iiit-h-intel-the-government-of-telangana-and-phfi-we-applied-ai-to-improve-early-detection-of-oral-cancer-through-deep-learning-and-image-analysis-grateful-to-be-part-of-impactful-healthcare-research-using-ai",
           title: 'Thrilled to share that our paper, “Screening of Oral Potentially Malignant Disorders and...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
